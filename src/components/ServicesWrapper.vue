fa65a44c6d01ad7512ac6edee9369edc9e05cd54
Allaniyazov Sultanbek <allaniyazov.sultan@mail.ru>
2024-07-30 00:52:55 +0500

    55

--- src/components/ServicesWrapper.vue
+++ src/components/ServicesWrapper.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="servicesWrapper" class="flex flex-wrap overflow-visible whitespace-nowrap relative gap-[1rem]">
         <div id="marker" :style="markerStyle"
-            class="absolute text-[transparent] bg-white rounded-full p-2 px-4 text-[1.5rem] font-medium">
-            abc
+            class="absolute text-[transparent] bg-white rounded-full p-2 px-4 text-[1rem] md:text-[1.5rem] h-fit font-medium">
+            
         </div>
         <service-card v-for="service in $tm('whatWeOffer.services')" :key="service" :name="service"
             class="service-card text-[1rem] md:text-[1.5rem] rounded-full bg-[hsla(0,0%,7%,.25)] hover:bg-transparent p-2 px-4 h-fit text-white cursor-pointer font-medium z-10 hover:z-0"
@@ -22,8 +22,8 @@ import { ref } from 'vue'
 const markerStyle = ref({
     top: '0px',
     left: '0px',
-    right: '0px',
     width: '0px',
+    height: '0px',
     opacity: '0'
 });
 
@@ -32,12 +32,13 @@ const moveMarker = (e) => {
     markerStyle.value = {
         top: `${target.offsetTop}px`,
         left: `${target.offsetLeft}px`,
-        right: `${target.offsetRight}px`,
         width: `${target.offsetWidth}px`,
+        height: `${target.offsetHeight}px`,
         opacity: '1'
         // display: 'block'
     };
     target.style.color = '#000'
+    target.style.backgroundColor = '#fff'
 };
 
 const resetMarker = (e) => {
@@ -47,6 +48,7 @@ const resetMarker = (e) => {
         opacity: '0'
     };
     target.style.color = '#fff'
+    target.style.backgroundColor = 'hsla(0,0%,7%,.25)'
     target.style.zIndex = 100
 };
 </script>
@@ -57,15 +59,16 @@ const resetMarker = (e) => {
 }
 
 #marker {
-    transition: top 200ms linear, left 200ms linear, opacity 100ms linear, width 100ms linear;
+    transition: top 200ms linear, left 200ms linear, opacity 100ms linear, width 100ms linear, height 100ms linear;
 }
 
 #service {
     white-space: normal;
+    transition: background-color 0s linear 0ms;
 }
 
-#service {
-    transition: color 10ms linear 10ms;
+#service:hover {
+    transition: color 10ms linear 10ms, background-color 0s linear 300ms;
 }
 
 
